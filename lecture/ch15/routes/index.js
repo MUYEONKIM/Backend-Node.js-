94cc8d6fbb10e9519df0147aa66e1b207edd812c
MUYEONKIM <rlaandus2@gmail.com>
2023-12-18 23:43:59 +0900

    Sequleize ORM

--- lecture/ch15/routes/index.js
+++ lecture/ch15/routes/index.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
-// GET / 라우터
-router.get('/', (req, res) => {
-  res.send('Hello, Express');
-});
+// 라우터 그룹화 하기
+// router.get('/', (req, res) => {
+//   res.send('Hello, Express');
+// });
+
+// router.post('/', (req, res) => {
+//   res.send('post hello')
+// })
+
+router.route('/')
+  .get((req, res) => {
+    res.send('GET Hello, express');
+  })
+  .post((req, res) => {
+    res.send('POST HELLO EXPRESS');
+  });
 
 module.exports = router;
